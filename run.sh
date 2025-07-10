7b528788f32184e65e16915f93eedaf3071e8c0c
qiangpan2 <qiangpan@amd.com>
2025-07-10 06:42:59 +0000

    ncu flashattn kernel added

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-nsys profile --trace=cuda,cublas,osrt,nvtx --gpu-metrics-devices=all --cuda-memory-usage=true --force-overwrite=true --output  flash_attn_profile pytest ./tests/test_flash_attn.py -k test_flash_attn_varlen_output -s
+#nsys profile --trace=cuda,cublas,osrt,nvtx --gpu-metrics-devices=all --cuda-memory-usage=true --force-overwrite=true --output  flash_attn_profile pytest ./tests/test_flash_attn.py -k test_flash_attn_varlen_output -s
+ncu --target-processes all \
+    --kernel-name ""flash_fwd_kernel \
+    --set full \
+    --export flash_attn_ncu_profile \
+    pytest ./tests/test_flash_attn.py -k test_flash_attn_varlen_output -s
